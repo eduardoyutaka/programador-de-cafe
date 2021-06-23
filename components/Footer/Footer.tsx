44ceade03c95f9af25f4d5684bea4182b92acb26
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2021-06-22 22:28:32 -0300

    Configure tailwind and create Footer component

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <footer className="h-96 bg-gray-700 text-white">
+      <ul>
+        <li>
+          <a href="https://github.com/eduardoyutaka">Github</a>
+        </li>
+        <li>
+          <a href="https://instagram.com/edu.nakanishi">Instagram</a>
+        </li>
+        <li>
+          <a href="https://vm.tiktok.com/ZMdUVNQkj/">TikTok</a>
+        </li>
+        <li>
+          <a href="https://twitch.tv/eduardo_nakanishi">Twitch</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'tailwindcss/tailwind.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Programador de Café</title>
+        <meta
+          name="description"
+          content="Programming and gaming personal website"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
-export default MyApp
+export default MyApp;
